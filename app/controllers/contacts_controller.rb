e2932a291f06794c98b977d542ccc13953d2c959
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-04-19 16:09:36 +0400

    Save contact to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,15 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    @contact.save
   end
-  
+
+  private  # Ниже будут храниться спрятанные (privat) методы, которые могут быть вызваны только из методов выше. Эти спрятанные методы не доступны извне. Это в том числе сделано для безопасности. 
+
+  def contact_params
+    params.require(:contact).permit(:email, :message)
+    # параметры.требуется(:контакт).разрешать(:электронную почту, :сообщение)
+  end
+
 end
