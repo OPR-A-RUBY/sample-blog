98c80c54fe7bbd8ff75611fde29c9b579722b834
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-04-19 20:52:08 +0400

    Maked article#create, save db, validation, display errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,20 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect  
+    @article = Article.new(contact_params)
+    if @article.valid?
+      @article.save
+    else
+      render action: 'new' 
+    end
+    # render plain: params[:article].inspect  
+  end
+
+  private  # Ниже будут храниться спрятанные (privat) методы, которые могут быть вызваны только из методов выше. Эти спрятанные методы не доступны извне. Это в том числе сделано для безопасности. 
+
+  def contact_params
+    params.require(:article).permit(:title, :text)
+    # параметры.требуется(:контакт).разрешать(:электронную почту, :сообщение)
   end
 
 end
