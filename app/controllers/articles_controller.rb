cd2d2c223e89cc001d2a3e3e2e1705e73e0ca8e1
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-04-19 22:27:52 +0400

    Add Help-rows, delete create.html.erb (list help-rows to articles#create)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,14 +4,20 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(article_params)
-    if @article.valid?
-      @article.save
-      redirect_to @article
-    else
-      render action: 'new' 
+
+    @article = Article.new(article_params)  # Создаёт экземпляр сущности Article
+                                            # Данные получаем из формы, причём: разрешаем их для использования в процедуре article_params см. ниже в спрятанном разделе кода (после private).
+
+    if @article.valid?                      # Если данные валидны, то 
+      @article.save                         # .. записать данные в БД и ..
+      redirect_to @article                  # .. перенаправить на ID (=11) из @article т.е.
+                                            # /articles/11/
+    else                    # ... иначе 
+      render action: 'new'  # перенаправить на акцию new, т.е. /articles/new/
     end
-    # render plain: params[:article].inspect  
+    # !!ВАЖНО!! - представление create.html.erb - не понадобится, т.к. оно не вызывается в этом методе. В результате выполнения будет вызвано одно из вышеописанных представлений.
+
+    # render plain: params[:article].inspect # Этоя для отладки  
   end
 
   private  # Ниже будут храниться спрятанные (privat) методы, которые могут быть вызваны только из методов выше. Эти спрятанные методы не доступны извне. Это в том числе сделано для безопасности. 
