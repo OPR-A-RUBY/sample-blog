103d22440bb3f2162502a29f91008d9b5398ea87
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-04-21 00:22:32 +0400

    Implement articles#edit and articles#update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)  # Создаёт экземпляр сущности Article
                                             # Данные получаем из формы, причём: разрешаем их для использования в процедуре article_params см. ниже в спрятанном разделе кода (после private).
 
+    # Специальную проверку валидности можно не делать, т.к. .save делает валидацию "автоматом". 
     if @article.valid?                      # Если данные валидны, то 
       @article.save                         # .. записать данные в БД и ..
       redirect_to @article                  # .. перенаправить на ID (=11) из @article т.е.
@@ -28,6 +29,23 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect # Этоя для отладки  
   end
 
+  def edit
+    @article = Article.find(params[:id])  # берём только ту статью, которую будем РЕДАКТИРОВАТЬ. Причём id статьи возьмём из адреса, который сформировала ссылка 'Edit' на странице index .
+
+  end
+
+  def update
+    @article = Article.find(params[:id])  # берём только ту статью, которую будем ОБНОВЛЯТЬ. Причём id статьи возьмём из ракаметра, переданного из формы /articles/11/edit' 
+
+    if @article.update(article_params)      # Функция update выполняет валидацию, как и save 
+      redirect_to @article                  # .. перенаправить на ID (=11) из @article т.е.
+                                            # /articles/11/
+    else                     # ... иначе 
+      render action: 'edit'  # перенаправить на акцию edit, т.е. /articles/11/edit/
+    end
+  end
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   private  # Ниже будут храниться спрятанные (privat) методы, которые могут быть вызваны только из методов выше. Эти спрятанные методы не доступны извне. Это в том числе сделано для безопасности. 
 
   def article_params
