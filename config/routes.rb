a269a8650ffcc6a6e6589c17708cab766cd8eb4f
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-04-28 23:31:05 +0400

    Add routes comments to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,15 @@ Rails.application.routes.draw do
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
 
+  # Создаём маршруты для единичного ресурса (resource)
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
-  resources :articles
-  
+  #                    только new и create маршруты 
+  #                                          маршрут new перенаправить на ''   ???
+
+  # Создаём маршруты для множественного ресурса (resources)
+  resources :articles do    # Каждой статье будет определён ...
+    resources :comments     # ... полный комплект маршрутов для коментариев
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
