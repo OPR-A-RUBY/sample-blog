07637750086df089c9d50bc23a29e3a59cb642c2
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-01 22:37:46 +0400

    Add RU-comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   validates :title, presence: true  # Валидация на непустое поле
   validates :text, presence: true   # ... аналогично.
   has_many :comments                # Связь с молделью Comment
+  # "имеет много : коментариев"
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :article     # Связь с молделью Article
+  # "принадлежит : статье"
 end
