d206f44e1f72afc474e0b02a90c4d72179977174
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-09 22:45:54 +0400

    Add validates to Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
+  validates :autor, presence: true
+  validates :body, presence: true
   belongs_to :article     # Связь с молделью Article
   # "принадлежит : статье"
 end
