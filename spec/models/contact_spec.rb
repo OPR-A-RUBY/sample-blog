8cb2dbc710c5ce722fe532092632cfb89081b196
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-09 14:59:32 +0400

    Added first condition

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,8 +1,9 @@
 require "spec_helper" do
 
-	describe "does something" do
+	describe Contact do
 
-	 # ...
+	 it { should validate_presence_of :email }
+   # должно проверять наличие адреса_электронной_почты
 
   end
    
