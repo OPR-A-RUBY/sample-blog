c379bffd82b120be77ca82410785fcd4a53308e5
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-09 22:44:38 +0400

    Edit contact_spec.rb

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it 'control_error_' do
+    should validate_presence_of( :email )
+  end  
 end
