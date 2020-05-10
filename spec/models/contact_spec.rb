53f75fa6cd90d961f67985fb3919f921b27179fa
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-10 22:08:02 +0400

    Create first test (it errored test)
    Реализован первый тест, хоть как-то обрабатываемый командой
    # rspec spec/models/contact_spec.rb
    # bundle exec rspec
    Хоть тест и ошибочный ... Увы!

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Contact, type: :model do
+# RSpec.describe Contact, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
-  it 'control_error_' do
-    expect(should validate_presence_of( :email )).to eq true
-  end  
+
+#   it 'control_error_' do
+#     expect(should validate_presence_of( :email )).to eq true
+#   end  
+# end
+
+RSpec.describe 'Contact' do        #
+  context 'before publication' do  # (almost) plain English
+    it 'cannot have email' do   #
+      # expect { Contact.create.email.create! }.to raise_error(ActiveRecord::RecordInvalid)  # test code
+      expect(Contact.create.email.present?).to eq false  # test code
+    end
+  end
 end
