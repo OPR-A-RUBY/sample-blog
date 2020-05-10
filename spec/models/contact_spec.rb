c059212b8f4e902fecbd07129e7aa7b335fe158e
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-10 22:59:36 +0400

    Test help

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Contact do          #
-  context 'before publication' do  # Название группы тестов
+  context 'before publication' do  # Название группы тестов 
     
     it 'cannot have email' do      # Первый тест: то что тестирует этот блок (1)
       # expect { Contact.create.email.create! }.to raise_error(ActiveRecord::RecordInvalid)  # test code
       expect(Contact.create.email.present?).to eq false  # test code
     end
 
-    it 'something doit' do        # Второй тест: то что тестирует этот блок (1)
-      expect(true).to eq true     # test code - всегда true
+    it 'something doit' do        # Второй тест: то что тестирует этот блок (2)
+      expect(true).to eq true     # test code - он всегда true
     end
+
   end
 end
