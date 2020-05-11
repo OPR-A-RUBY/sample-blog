187ed449c3f443908bd7d290c531539f46aba8ba
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-11 23:31:30 +0400

    Add a test that does nothing.

---
+++ spec/features/visitor_creates_contact_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+# feature "Contact creation" do
+#   scenario "allows acess to contacts page" do
+#     visit new_contacts_path
+
+#     expect(page).to have_content 'Contact us'
+#   end
+# end
+
+feature "..." do
+  scenario "..." do
+
+  end
+end
