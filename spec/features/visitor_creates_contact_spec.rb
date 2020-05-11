3f6c52c127f62684f273bcd6cddde863a37b1809
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-12 01:01:19 +0400

    #47 = 0:36:00
    Прописали такую же ссылку в тестовый файл.

--- spec/features/visitor_creates_contact_spec.rb
+++ spec/features/visitor_creates_contact_spec.rb
@@ -4,7 +4,7 @@ feature "Contact creation" do
   scenario "allows acess to contacts page" do
     visit new_contacts_path
 
-    expect(page).to have_content 'Contacts page'
+    expect(page).to have_content I18n.t('contacts.contact_us')
   end
 end
 
