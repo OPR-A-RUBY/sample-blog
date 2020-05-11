c1f99474063b4a329f222bd5fa1972fff48a2c21
OPR-A-RUBY <opr.a.ruby@gmail.com>
2020-05-12 00:09:31 +0400

    Fill the file with test lines
    Добавили две команды:
    visit new_contacts_path  # Посетим страницу
    expect(page).to have_content 'Contacts page' # Ожидаем, что страница есть имеет контекст указанный в кавычках.

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -2,4 +2,5 @@ FactoryGirl.define do
   factory :contact do
     email "email@email.ru"
     text "Text information."
+  end  
 end
--- spec/features/visitor_creates_contact_spec.rb
+++ spec/features/visitor_creates_contact_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 
-# feature "Contact creation" do
-#   scenario "allows acess to contacts page" do
-#     visit new_contacts_path
+feature "Contact creation" do
+  scenario "allows acess to contacts page" do
+    visit new_contacts_path
 
-#     expect(page).to have_content 'Contact us'
-#   end
-# end
-
-feature "..." do
-  scenario "..." do
-
+    expect(page).to have_content 'Contacts page'
   end
 end
+
+# feature "..." do
+#   scenario "..." do
+
+#   end
+# end
